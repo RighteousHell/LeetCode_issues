cb8e088a4ddea425a94bae0c6caae4a146d29f28
RighteousHell <Strahov.PG@gmail.com>
2022-01-17 20:24:17 +0300

    Time: 18 ms (5.60%), Space: 10 MB (79.60%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -8,21 +8,10 @@ public:
         
         for ( int i = 0; i < nums.size(); i+=2 )
         {
-            /*if ( nums.at(i)  > nums.at(i+1) )
-            {
-                val = nums.at(i);
-                freq = nums.at(i+1); 
-                
-            }
-            else 
-            {
-                val = nums.at(i+1);
-                freq = nums.at(i);
-            }*/
+
             freq = nums.at(i);
             val = nums.at(i+1);
             
-            cout << " freq = " << freq << ";  val = " << val << endl;
             
             
             for (int j = 0; j < freq; ++j)
