9bd1ffce0a335cd9f1cb5ec69a25ad82bbfda035
RighteousHell <Strahov.PG@gmail.com>
2022-02-04 12:46:04 +0300

    Time: 363 ms (5.05%), Space: 33.6 MB (46.38%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> answer;
+        
+        int count  = 0;
+        //bool flag = false;
+        
+        
+        sort(nums.begin(), nums.end());
+        int curr = nums.at(0);
+        
+        for (auto& tmp: nums)
+        {
+            cout << tmp << " ";
+        }
+        for (int i = 1; i < nums.size(); ++i)
+        {
+              
+            if (curr == nums.at(i) )
+            {
+                ++count;
+            }
+            if ((curr != nums.at(i)) && (0 < count))
+            {
+                answer.emplace_back(curr);
+                count = 0;
+            }
+            curr = nums.at(i);
+        }
+    
+        if (0 < count )
+            {
+                answer.emplace_back(curr);
+            }
+        
+    return answer;    
+    }
+};
