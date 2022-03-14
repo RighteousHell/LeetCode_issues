76305c2fff92b38579733355211c04b2b7edc6e4
RighteousHell <Strahov.PG@gmail.com>
2022-03-14 21:33:09 +0300

    Time: 32 ms (15.84%), Space: 16.9 MB (19.24%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-                    vector<int> tmp;
-            for (int i = 0; i< nums.size(); ++i)
+        vector <int> answer;
+        
+        
+        for ( int i = 0; i < nums.size(); ++i )
         {
-               tmp.emplace_back(nums.at(nums.at(i)));
+            answer.emplace_back(nums.at(nums.at(i)));
         }
-        return tmp;
-        
-        
+     
+        return answer;
     }
 };
