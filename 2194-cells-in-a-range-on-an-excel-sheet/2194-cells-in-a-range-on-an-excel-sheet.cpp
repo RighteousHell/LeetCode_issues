cbcd00ab1ba71f004ed02f37657e2c33214763e3
RighteousHell <Strahov.PG@gmail.com>
2022-04-05 16:15:25 +0300

    Time: 8 ms (24.16%), Space: 7.9 MB (19.56%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -4,13 +4,11 @@ public:
         const std::string delim = ":";
         std::string source_pos, dest_pos;
         
-        source_pos = s.substr(0, s.find(delim));
-        dest_pos = s.substr(s.find(delim) + delim.length(), 4);
 
         vector<string> answer;
-        for (char i = source_pos[0]; i <= dest_pos[0]; ++i)
+        for (char i = s[0]; i <= s[3]; ++i)
         {
-            for (char j = source_pos[1]; j <= dest_pos[1]; ++j)
+            for (char j = s[1]; j <= s[4]; ++j)
             {
                 cout << i << " + " << j;
                 answer.emplace_back(std::string("") + i + j);
