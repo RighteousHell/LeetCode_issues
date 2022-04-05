ba91f8f0ede6c38b28872c8fbf39ee14db5ca3f7
RighteousHell <Strahov.PG@gmail.com>
2022-04-05 16:17:13 +0300

    Time: 32 ms (6.87%), Space: 7.8 MB (19.56%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -8,10 +8,11 @@ public:
         vector<string> answer;
         for (char i = s[0]; i <= s[3]; ++i)
         {
+            std::string tmp = "";
             for (char j = s[1]; j <= s[4]; ++j)
             {
                 cout << i << " + " << j;
-                answer.emplace_back(std::string("") + i + j);
+                answer.emplace_back(tmp + i + j);
             }
         }
         
