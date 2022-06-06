c058cdebec68b0c4f7ecce8e7b34b19273fcab34
RighteousHell <Strahov.PG@gmail.com>
2022-06-06 21:09:41 +0300

    Time: 161 ms (5.25%), Space: 30.9 MB (43.24%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    const multimap <string, int> items_specif {{"type", 0},{"color", 1},{"name", 2}};
+    
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        
+        cout << "RuleValue: " <<  ruleValue << endl;
+        cout << "ruleKey: " <<  ruleKey;
+        int ans = 0;
+        auto entity = items_specif.find(ruleKey);
+        
+        for (auto& tmp: items)
+        {
+            if (ruleValue == tmp.at(entity->second))
+            {
+                ++ans;
+            }
+        }
+        
+        
+        return ans;
+        
+    }
+};
