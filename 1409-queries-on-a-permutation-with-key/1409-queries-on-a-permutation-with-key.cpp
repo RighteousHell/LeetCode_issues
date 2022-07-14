45ffd067e38bb657379a6f6cfbf4d09b7ab8f46f
RighteousHell <Strahov.PG@gmail.com>
2022-07-14 16:44:33 +0300

    Time: 23 ms (49.56%), Space: 9.1 MB (16.16%) - LeetHub

---
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> processQueries(vector<int>& queries, int m) {
+        list<int> p;
+        for (int i = 1; i <= m; ++i)
+        {
+            p.emplace_back(i);
+        }
+        
+        vector<int> ans;
+        int index = 0;
+        for(int i = 0; i < queries.size(); ++i)
+        {
+            int poss = 0;
+            for (auto j = p.begin(); j !=  p.end(); ++poss)
+            {
+                if (*j == queries[i])
+                {
+                    
+                    ans.emplace_back(poss);
+                    int tmp = *j;
+                    p.erase(j);
+                    p.emplace_front(tmp);
+                    
+                    break;
+                }
+                else 
+                {
+                    ++j;
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+    }
+};
