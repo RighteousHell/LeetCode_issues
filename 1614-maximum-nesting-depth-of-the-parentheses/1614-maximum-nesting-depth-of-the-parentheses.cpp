9fe92c1163e25def116d19cc7fa6c5197f82ee01
RighteousHell <Strahov.PG@gmail.com>
2022-08-05 19:06:22 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (59.01%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxDepth(string s) {
         int cur_deep = 0;
-        int max_deep = 0;
         int ans = 0;
         
         for (auto& ch : s)
@@ -10,9 +9,9 @@ public:
             if (ch == '(')
             {
                 ++cur_deep;
-                 if ( cur_deep > max_deep )
+                 if ( cur_deep > ans )
                  {
-                ans = max_deep = cur_deep;
+                ans = cur_deep;
                  }
             }
             if (ch == ')')
