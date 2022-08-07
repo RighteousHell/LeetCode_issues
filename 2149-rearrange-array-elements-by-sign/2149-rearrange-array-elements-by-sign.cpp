ab3a08852aba31feaf4c00d3d79c05f39fc2dddb
RighteousHell <Strahov.PG@gmail.com>
2022-08-07 20:03:21 +0300

    Time: 333 ms (55.28%), Space: 123.9 MB (67.07%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> ans(nums.size());
-        for (int i = 0, j= 0; i < nums.size(); ++i)
+        /*for (int i = 0, j= 0; i < nums.size(); ++i)
         {
             if (nums[i] > 0)
             {   
@@ -19,6 +19,22 @@ public:
             }
         }
 
+        return ans;
+    }*/
+        for (int i = 0, j = 0, k = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] > 0)
+            {
+                ans[j] = nums[i];
+                j += 2;
+            } else
+            {
+                ans[k] = nums[i];
+                k += 2;
+                
+            }
+        }
+        
         return ans;
     }
 };
