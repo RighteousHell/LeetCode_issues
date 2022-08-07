182fa37831180622d0c663005aa7f2c831e510e4
RighteousHell <Strahov.PG@gmail.com>
2022-08-07 16:39:52 +0300

    Time: 2 ms (60.79%), Space: 6.5 MB (56.73%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        int i = 0;
+        for (auto& ch: s)
+        {
+            if (isspace(ch))
+            {
+                --k;
+                if (0 == k)
+                {
+                   break; 
+                }
+            }
+            ++i;
+            
+        }
+        
+        if (k)
+        {
+            ans = s;
+        }
+        else
+        {
+            ans = string(s.begin(), s.begin()+i);
+        }
+        
+        return ans;
+    }
+};
