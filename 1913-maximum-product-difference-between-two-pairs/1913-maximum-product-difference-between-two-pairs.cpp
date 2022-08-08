3c06c95f6d206c1fe58ac2ddddcbfe024f784430
RighteousHell <Strahov.PG@gmail.com>
2022-08-08 15:26:46 +0300

    Time: 327 ms (5.05%), Space: 20.5 MB (13.83%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -2,7 +2,44 @@ class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
 
-sort(nums.begin(), nums.end());        
+/*sort(nums.begin(), nums.end());        
         return (nums[nums.size()-1] * nums[nums.size()-2]) - (nums[0] * nums[1]); 
+        
+        */
+        
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+        int max1 = 0;
+        int max2 = 0;
+        
+        for (auto& tmp: nums)
+        {
+            if (tmp > max2)
+            {
+                max2 = tmp;
+                if (tmp > max1)
+                {
+                    max2 = max1;
+                    max1 = tmp;
+                }
+                
+                
+            }
+            if (tmp < min2)
+            {
+                min2 = tmp;
+                if (tmp < min1)
+                {
+                    min2 = min1;
+                    min1 = tmp;
+                }
+                
+                
+            }
+            cout << endl << "min1 " << min1 << " min2 " << min2;
+        }
+        
+        cout << max1 << " " << max2 << " " << min1 << " " << min2 << " ";
+        return (max1 * max2) - (min1 * min2);
     }
 };
