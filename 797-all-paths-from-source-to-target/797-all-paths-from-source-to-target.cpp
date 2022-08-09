7a955ddbc2fb7fd5448796b3322da1cbde9688c4
RighteousHell <Strahov.PG@gmail.com>
2022-08-09 19:34:48 +0300

    Time: 28 ms (46.16%), Space: 15.2 MB (33.92%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -12,13 +12,8 @@ public:
     void dfs( vector<vector<int>>& graph, vector<vector<int>>& ans, vector<int> some_path, int index)
     {
         some_path.emplace_back(index);
-        //cout << "some_path: ";
-        //for (auto& tmp: some_path)
-       // {
-          //  cout << tmp <<  " ";
-        //}
         
-        if (/*graph[index].empty()*/ index == graph.size()-1)
+        if ( index == graph.size()-1)
         {
             ans.emplace_back(some_path);
             return;
