c485f1903d373b6934a2ec06be93697772ccfe7e
RighteousHell <Strahov.PG@gmail.com>
2022-08-14 18:17:57 +0300

    Time: 3 ms (86.35%), Space: 8.8 MB (10.98%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        int ans = 0;
+        unordered_map<char, string> hash = {{'a',".-"},{'b', "-..."},{ 'c', "-.-."},{ 'd', "-.."},
+                                                      {'e', "."},{'f', "..-."},{'g', "--."},{'h', "...."},
+                                                      {'i', ".."},{'j', ".---"},{'k', "-.-"},{'l', ".-.."},
+                                                      {'m', "--"},{'n', "-."},{'o', "---"},{'p', ".--."},
+                                                      {'q', "--.-"},{'r', ".-."},{'s', "..."},{'t', "-"},
+                                                      {'u', "..-"},{'v', "...-"},{'w', ".--"} ,{'x', "-..-"},
+                                                      {'y', "-.--"},{'z', "--.."}};
+        unordered_set<string> uniq_strings;
+        
+        
+        for (auto& tmp: words)
+        {
+            string tmp_str;
+            for (auto& ch: tmp)
+            {
+                auto node_hash = hash.find(ch);
+                if (node_hash != hash.end())
+                {
+                    tmp_str += node_hash->second;
+                }
+            }
+            if (uniq_strings.find(tmp_str) == uniq_strings.end())
+            {
+                uniq_strings.emplace(tmp_str);
+            }
+        }
+    ans = uniq_strings.size();
+        return ans;
+
+    }
+};
