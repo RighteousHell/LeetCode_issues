128439e0b3679d738ef711546655865d8546fc13
RighteousHell <Strahov.PG@gmail.com>
2022-08-16 21:16:29 +0300

    Time: 7 ms (6.92%), Space: 6.1 MB (54.81%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if( isdigit(s[i]) )
+            {
+                s[i] = s[i-1] + s[i] - '0';
+            }
+        }
+        
+     return s;   
+    }
+};
