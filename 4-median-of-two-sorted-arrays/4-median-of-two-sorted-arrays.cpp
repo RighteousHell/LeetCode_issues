c52933874b94b109f935a9a7376f4af77f5473f2
RighteousHell <Strahov.PG@gmail.com>
2022-08-17 21:18:53 +0300

    Time: 75 ms (30.32%), Space: 89.6 MB (41.06%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double ans = 0.;
-        vector<int> merged_array;
-        merged_array.insert(merged_array.end(), nums1.begin(), nums1.end());
-        merged_array.insert(merged_array.end(), nums2.begin(), nums2.end());
-        sort(merged_array.begin(), merged_array.end());
+        double ans = 0.;        
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());    
         
-        for (auto& tmp: merged_array)
+        int size = nums1.size();
+        if (nums1.size()%2 == 0)
         {
-            cout << tmp << " ";
-        }
-        
-        
-        int size = merged_array.size();
-        if (merged_array.size()%2 == 0)
-        {
-            ans = (merged_array[size/2] + merged_array[size/2-1])/2.0;
+            ans = (nums1[size/2] + nums1[size/2-1])/2.0;
         }
         else 
         {
-            ans = merged_array[size/2];
+            ans = nums1[size/2];
         }
         return ans;
         
